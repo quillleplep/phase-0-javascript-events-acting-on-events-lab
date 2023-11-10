e0a6fa19d8eb3cccf959f5c08b9ea29eab5c988b
quill_nichols <leplep00100@gmail.com>
2023-11-09 20:28:24 -0500

    move

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+const DODGER = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    // Move the DODGER to the left
+    const leftPosition = parseInt(DODGER.style.left.replace('px', ''), 10);
+    if (leftPosition > 0) {
+        DODGER.style.left = `${leftPosition - 10}px`; // Adjust the value to your desired movement
+    }
+}
+
+function moveDodgerRight() {
+    // Move the DODGER to the right
+    const leftPosition = parseInt(DODGER.style.left.replace('px', '10'), 10);
+    const rightBoundary = 1000000/* Set the right boundary value */;
+    if (leftPosition < rightBoundary) {
+        DODGER.style.left = `${leftPosition + 4}10px`; // Adjust the value to your desired movement
+    }
+}
